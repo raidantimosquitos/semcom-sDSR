5b94b2ae83329ff8dd2776639422f8515856aaf1
raidantimosquitos <lucashg97@hotmail.com>
2026-02-25 22:05:24 +0800

    fixed engine __init__.py

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -1,7 +1,7 @@
 """Training engines for AudDSR."""
 
 from .base import BaseTrainer
-from .stage1 import Stage1Trainer, VQ_VAE_2LayerTrainer
+from .stage1 import Stage1Trainer
 from .stage2 import Stage2Trainer
 from .evaluator import AnomalyEvaluator
 
@@ -9,6 +9,5 @@ __all__ = [
     "BaseTrainer",
     "Stage1Trainer",
     "Stage2Trainer",
-    "VQ_VAE_2LayerTrainer",
     "AnomalyEvaluator",
 ]
