758aa02b1bcae1527545bac32ac15eee9b39b7ab
raidantimosquitos <lucashg97@hotmail.com>
2026-02-26 16:38:19 +0800

    tests1

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -99,6 +99,8 @@ def _run_evaluation(args: argparse.Namespace, tee: Callable[[str], None]) -> Non
     )
     model = build_s_dsr(n_mels, T, vq_vae)
     ckpt = torch.load(args.ckpt, map_location="cpu", weights_only=True)
+    print(ckpt.keys())
+    exit()
     model.load_state_dict(ckpt["model_state_dict"])
 
     evaluator = AnomalyEvaluator(
