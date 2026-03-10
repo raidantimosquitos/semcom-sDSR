0668f19d0c94a8699da36396e2a8cccf04e9f8dd
raidantimosquitos <lucashg97@hotmail.com>
2026-03-10 20:25:03 +0800

    Inverted shapes in fine/coarse

--- src/models/vq_vae/encoders.py
+++ src/models/vq_vae/encoders.py
@@ -15,8 +15,8 @@ class EncoderFine(nn.Module):
                                  stride=2, padding=1)
         self._conv_2 = nn.Conv2d(in_channels=num_hiddens//2,
                                  out_channels=num_hiddens,
-                                 kernel_size=3,
-                                 stride=1, padding=1)
+                                 kernel_size=4,
+                                 stride=2, padding=1)
         self._conv_3 = nn.Conv2d(in_channels=num_hiddens,
                                  out_channels=num_hiddens,
                                  kernel_size=3,
