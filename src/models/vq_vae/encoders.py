9b57898f360b2cb2576eb3cb2e837cb002b4c631
raidantimosquitos <lucashg97@hotmail.com>
2026-03-10 19:43:05 +0800

    Inverted shapes in fine/coarse

--- src/models/vq_vae/encoders.py
+++ src/models/vq_vae/encoders.py
@@ -13,10 +13,36 @@ class EncoderFine(nn.Module):
                                  out_channels=num_hiddens // 2,
                                  kernel_size=4,
                                  stride=2, padding=1)
-        self._conv_2 = nn.Conv2d(in_channels=num_hiddens // 2,
+        self._conv_2 = nn.Conv2d(in_channels=num_hiddens//2,
                                  out_channels=num_hiddens,
+                                 kernel_size=3,
+                                 stride=1, padding=1)
+        self._residual_stack = ResidualStack(in_channels=num_hiddens,
+                                             num_hiddens=num_hiddens,
+                                             num_residual_layers=num_residual_layers,
+                                             num_residual_hiddens=num_residual_hiddens)
+
+    def forward(self, inputs):
+        x = self._conv_1(inputs)
+        x = F.relu(x)
+
+        x = self._conv_2(x)
+
+        return self._residual_stack(x)
+
+
+class EncoderCoarse(nn.Module):
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(EncoderCoarse, self).__init__()
+
+        self._conv_1 = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=num_hiddens // 2,
                                  kernel_size=4,
                                  stride=2, padding=1)
+        self._conv_2 = nn.Conv2d(in_channels=num_hiddens // 2,
+                                 out_channels=num_hiddens,
+                                 kernel_size=3,
+                                 stride=1, padding=1)
         self._conv_3 = nn.Conv2d(in_channels=num_hiddens,
                                  out_channels=num_hiddens,
                                  kernel_size=3,
@@ -32,34 +58,7 @@ class EncoderFine(nn.Module):
 
         x = self._conv_2(x)
         x = F.relu(x)
-
+        
         x = self._conv_3(x)
-        return self._residual_stack(x)
-
-
-class EncoderCoarse(nn.Module):
-    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
-        super(EncoderCoarse, self).__init__()
-
-        self._conv_1 = nn.Conv2d(in_channels=in_channels,
-                                 out_channels=num_hiddens,
-                                 kernel_size=4,
-                                 stride=2, padding=1)
-        self._conv_2 = nn.Conv2d(in_channels=num_hiddens,
-                                 out_channels=num_hiddens,
-                                 kernel_size=3,
-                                 stride=1, padding=1)
-        self._residual_stack = ResidualStack(in_channels=num_hiddens,
-                                             num_hiddens=num_hiddens,
-                                             num_residual_layers=num_residual_layers,
-                                             num_residual_hiddens=num_residual_hiddens)
-
-    def forward(self, inputs):
-        x = self._conv_1(inputs)
-        x = F.relu(x)
-
-        x = self._conv_2(x)
-        x = F.relu(x)
-
         x = self._residual_stack(x)
         return x
