14fa0a587ffac8fa5ca40d1175dc38b61a234b45
raidantimosquitos <lucashg97@hotmail.com>
2026-04-20 13:21:54 +0800

    architecture tries on vqvae and sDSR

--- src/models/vq_vae/res_blocks_2d.py
+++ src/models/vq_vae/res_blocks_2d.py
@@ -7,12 +7,10 @@ class Residual(nn.Module):
     def __init__(self, in_channels, num_hiddens, num_residual_hiddens):
         super(Residual, self).__init__()
         self._block = nn.Sequential(
-            nn.BatchNorm2d(in_channels), 
             nn.ReLU(False),
             nn.Conv2d(in_channels=in_channels,
                       out_channels=num_residual_hiddens,
                       kernel_size=3, stride=1, padding=1, bias=True),
-            nn.BatchNorm2d(num_residual_hiddens),
             nn.ReLU(False),
             nn.Conv2d(in_channels=num_residual_hiddens,
                       out_channels=num_hiddens,
